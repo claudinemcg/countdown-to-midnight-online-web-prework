00eef8a7c579be1d54d9eb65ebfcd1e8d0230589
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-19 15:54:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,12 +3,17 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
   end
+<<<<<<< HEAD
   return "HAPPY NEW YEAR!"
+=======
+  puts "HAPPY NEW YEAR!"
+>>>>>>> b03366abc598e3c6e4395c5dbc60af8299279c3b
 end
 
 
 def countdown_with_sleep(number)
   while number > 0
+<<<<<<< HEAD
     sleep 1  #wait 1 second
     puts "#{number} SECOND(S)!"
     number -= 1
@@ -18,3 +23,12 @@ def countdown_with_sleep(number)
 end
 
 
+=======
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+countdown(5)
+>>>>>>> b03366abc598e3c6e4395c5dbc60af8299279c3b
